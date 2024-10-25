17cba47962d86a54b63c3fcd696b3d60d125ce11
Тхатель Алибек <alibek.tkhatel@mail.ru>
2024-10-26 01:28:50 +0300

    successful 2st task

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,14 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if (name == null) return false; //проверка на нажатие кнопки "Отмена"
+  else {
+    let isValidSpaсe = name.trim().includes(' '); //проверка на наличие внутренних пробелов в строке при наличие внешних
+
+    if (isValidSpaсe || name.split() == '' || name.length < 4 || name == null)
+      return false; //проверка на пустую строку и минимальную длину строки
+    else return true;
+  }
 }
 
 function sayHello() {
