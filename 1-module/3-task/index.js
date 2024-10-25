77adac2d9d0f1ecdd10ff7a8148889b490fbe85e
Тхатель Алибек <alibek.tkhatel@mail.ru>
2024-10-26 01:42:49 +0300

    successful 3st task

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,6 @@
 function ucFirst(str) {
-  // ваш код...
+  if (str == '') return str //проверка на пустую строку
+
+  let newString = str[0].toUpperCase() + str.slice(1, str.length) //берем первый символ в верхнем регистре и записываем в новую строку + всю оставшуюся строку
+  return newString // возвращаем новую строку
 }
