daf2468e1be11343b272bad3991818cf3ccf4066
Тхатель Алибек <alibek.tkhatel@mail.ru>
2024-10-26 01:56:10 +0300

    successful 4st task

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,9 @@
 function checkSpam(str) {
-  // ваш код...
+  let chekOf1Xbet = str.toLowerCase().includes('1xbet'); //что бы игнорировать регистр и его комбинации просто приводим все к нижнему и ищем подстроку 1xbet
+  let chekOfXXX = str.toLowerCase().includes('xxx'); //и подстроку xxx
+
+  if (chekOf1Xbet || chekOfXXX) { // при любом совпадении выводим true
+    return true;
+  }
+  return false;
 }
