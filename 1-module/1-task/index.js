504084d8008af65df4aff1faeaa3ab3a734bde87
Тхатель Алибек <alibek.tkhatel@mail.ru>
2024-10-26 00:12:45 +0300

    successful 1st task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
-  // ваш код...
+  let resultFactorial = n; //создаем корзину куда сразу кладем максимальный множитель равный основанию факториала
+  if (n !== 0) {  //проверка на НЕ ноль, в противном случае просто возвращаем единицу
+    for (let i = 1; i < n; i++) {
+      resultFactorial *= (n-i); //в корзину попадает каждый раз произведение изначального значения на все значения меньше
+    }
+    return resultFactorial;
+  } else {
+    resultFactorial = 1;
+    return resultFactorial;
+  }
 }
