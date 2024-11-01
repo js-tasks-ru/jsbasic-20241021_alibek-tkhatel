e1f55e556c29f62eeb0e606cd05c9bfde9117b4b
Тхатель Алибек <alibek.tkhatel@mail.ru>
2024-11-02 02:25:07 +0300

    successful 1st task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,15 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let totalSalary = 0; //задаем изначальное значение аккумулятора (при отсутствии зарплат, останется нулем)
+
+  for (let key in salaries) {
+    if (
+      typeof salaries[key] == 'number' && //отсекаем варианты ТОЛЬКО ЧИСЛО, не равно NaN и варианты с бесконечностью
+      !isNaN(salaries[key]) &&
+      salaries[key] !== Infinity &&
+      salaries[key] !== -Infinity
+    ) {
+      totalSalary += salaries[key]; //при выполнении условия, складываем итерируемые значения свойств объекта в аккумулятор
+    }
+  }
+  return totalSalary;
 }
