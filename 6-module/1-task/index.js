68425b7029a3d86139b1de3dc53cf1d193b075ab
Тхатель Алибек <alibek.tkhatel@mail.ru>
2024-11-26 02:16:11 +0300

    successful 1st task from 6-module

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,50 @@
  *
  */
 export default class UserTable {
+  elem = null;
+  rows = [];
   constructor(rows) {
+    this.rows = rows || this.rows;
+    this.#render();
   }
+
+  #template() {
+    return `
+    <table>
+    ${this.rows
+      .map(
+        (item) =>
+          `<tr class= 'table-row'><td>${item.name}</td><td>${item.age}</td><td>${item.salary}</td><td>${item.city}</td><td><button data-remove = true>X</button></td></tr>`,
+      )
+      .join('\n')}
+    </table>`;
+  }
+
+  #onRowRemove = () => {
+    const event = new CustomEvent('remove-row', {
+      bubbles: true,
+    });
+  };
+
+  #render() {
+    this.elem = createElement(this.#template());
+
+    this.elem.addEventListener('click', ({ target }) => {
+      if (target.dataset.remove !== 'true') {
+        return;
+      }
+      const row = target.closest('.table-row');
+      if (!row) {
+        return;
+      }
+      row.remove();
+      console.log(target);
+    });
+  }
+}
+
+function createElement(html) {
+  const temp = document.createElement('div');
+  temp.innerHTML = html;
+  return temp.firstElementChild;
 }
